a62979d352144284d3fef611cc85183c3f42a6da
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-08-14 18:36:05 -0300

    feat: add list by id and insert methods

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,7 +7,7 @@ import (
 	"log"
 )
 
-func Listar() {
+func List() {
 	connect.Connect()
 	sql := "SELECT id, nombre, correo, telefono FROM clientes ORDER BY id DESC;"
 	datos, err := connect.Db.Query(sql)
@@ -35,4 +35,38 @@ func Listar() {
 			dato.Correo, dato.Telefono)
 		fmt.Println("---------------------------------------------------------")
 	}
+}
+
+func ListById(id int) {
+	connect.Connect()
+	sql := "SELECT id, nombre, correo, telefono FROM clientes where id=?;"
+	datos, err := connect.Db.Query(sql, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connect.CloseConnection()
+	fmt.Println("\nLISTAR CLIENTE POR ID")
+	fmt.Println("---------------------------------------------------------")
+	for datos.Next() {
+		var dato models.Cliente
+		err := datos.Scan(&dato.Id, &dato.Nombre, &dato.Correo, &dato.Telefono)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Id: %v | Nombre: %v | Email: %s | Teléfono: %s \n", dato.Id, dato.Nombre,
+			dato.Correo, dato.Telefono)
+		fmt.Println("---------------------------------------------------------")
+	}
+}
+
+func Insert(cliente models.Cliente) {
+	connect.Connect()
+	sql := "INSERT INTO clientes values(null, ?, ?, ?);"
+	result, err := connect.Db.Query(sql, cliente.Nombre, cliente.Correo, cliente.Telefono)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	fmt.Println("Se creó el registro exitosamente")
 }
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "clase_3_mysql_driver/handlers"
+import (
+	"clase_3_mysql_driver/handlers"
+	"clase_3_mysql_driver/models"
+)
 
 func main() {
-	handlers.Listar()
+	// handlers.List()
+	// handlers.ListById(1)
+	cliente := models.Cliente{Nombre: "Marcos Ñamandu", Correo: "marcos@hotmail.com",
+		Telefono: "+96656898"}
+	handlers.Insert(cliente)
+	handlers.List()
 }
